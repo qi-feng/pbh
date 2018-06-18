a4dda1fd4c851224016b0b2507693a12fa1e569a
qi-feng <sely520@gmail.com>
2018-06-17 21:49:17 -0400

    Add tehanu condor submit

--- pbh.py
+++ pbh.py
@@ -94,7 +94,8 @@ class Pbh(object):
         self._burst_dict = {}  #{"Burst #": [event # in this burst]}, for internal use
         self.VERITAS_deadtime = 0.33e-3  # 0.33ms
         self.runNum = 0
-        self.data_dir = '/raid/reedbuck/qfeng/pbh/data'
+        #self.data_dir = '/raid/reedbuck/qfeng/pbh/data'
+        self.data_dir = '/a/data/tehanu/qifeng/pbh/EDroot_files/'
         #self.data_dir = '/global/cscratch1/sd/qifeng/data/pbh_data/'
 
     def read_photon_list(self, ts, RAs, Decs, Es, ELs):
@@ -2824,6 +2825,108 @@ def qsub_cori_runlist(filename="pbh_runlist.txt", window_size=10, plot=False, bk
             #isend_command = 'qsub -l nodes=reedbuck -q batch -V %s'%scriptfullname
             #isend_command = 'qsub -l nodes=%s -q batch -V %s'%(hostname, scriptfullname)
             isend_command = 'sbatch -C haswell %s'%(scriptfullname)
+
+
+
+            print(isend_command)
+            os.system(isend_command)
+            print("Run %d sent to queue." % run_num)
+        except:
+            print("*** Can't process run: %d ***" % run_num)
+            raise
+
+
+def qsub_tehanu_runlist(filename="pbh_runlist.txt", window_size=10, plot=False, bkg_method="scramble",
+                     script_dir = '/a/data/tehanu/qifeng/pbh', overwrite=True, hostname=None, walltime=48):
+    if window_size<1:
+        print('Submitting jobs for runlist %s with search window size %.1g' % (filename, window_size))
+    else:
+        print('Submitting jobs for runlist %s with search window size %.1f'%(filename, window_size))
+    #data_base_dir = '/raid/reedbuck/veritas/data/'
+    #script_dir = '/raid/reedbuck/qfeng/pbh/'
+
+    runlist_df = pd.read_csv(filename, header=None)
+    runlist_df.columns = ["runNum"]
+    runlist = runlist_df.runNum.values
+    if hostname is None:
+        hostname = socket.gethostname()
+    for run_num in runlist:
+        try:
+            pyscriptname = "pbh.py"
+            if window_size<1:
+                scriptname = 'pbhs_run%d_window_size%.4f-s.sh' % (run_num, window_size)
+                condor_scriptname = 'pbhs_run%d_window_size%.4f-s.condor' % (run_num, window_size)
+            else:
+                scriptname = 'pbhs_run%d_window_size%d-s.sh'%(run_num, window_size)
+                condor_scriptname = 'pbhs_run%d_window_size%d-s.condor' % (run_num, window_size)
+            #scriptname = 'pbhs_run%d_window_size%d-s.pbs'%(run_num, window_size)
+            scriptfullname = os.path.join(script_dir, scriptname)
+            condor_scriptname = os.path.join(script_dir, condor_scriptname)
+            pyscriptname = os.path.join(script_dir, pyscriptname)
+            pklname = "pbhs_bkg_method_"+str(bkg_method)+"_run"+str(run_num)+"_window"+str(window_size)+"-s.pkl"
+            #if os.path.exists(scriptfullname):
+            if os.path.exists(pklname):
+                #print('*** Warning: script already exists: %s ***'%scriptfullname)
+                print('*** Warning: pickle file already exists: %s ***'%pklname)
+                if not overwrite:
+                    print("Aborting")
+                    continue
+                    #sys.exit(1)
+                print('Overwriting...')
+            if window_size < 1:
+                logfilename = 'pbhs_run%d_window_size%.4f-s.log' % (run_num, window_size)
+            else:
+                logfilename = 'pbhs_run%d_window_size%d-s.log'%(run_num, window_size)
+            #logfilename = 'pbhs_run%d_window_size%d-s.log'%(run_num, window_size)
+            #script = open(scriptfullname, 'w')
+            with open(scriptfullname, 'w') as script:
+                script.write('#!/bin/bash  \n\n')
+                script.write('date \n')
+                script.write('hostname \n')
+                script.write('cd {} \n'.format(script_dir))
+                script.write('pwd \n')
+                script.write('shopt -s expand_aliases \n')
+                script.write('source /usr/nevis/adm/nevis-init.sh \n')
+                script.write('source /a/home/tehanu/qifeng/.bashrc \n')
+
+                if plot:
+                    #script.write('srun -n 1 -C haswell python %s -r %d -w %d -b %s -p >> %s\n'%(pyscriptname, run_num, window_size, bkg_method, logfilename))
+                    #script.write('python %s -r %d -w %d -b %s -p >> %s\n'%(pyscriptname, run_num, window_size, bkg_method, logfilename))
+                    if window_size<1:
+                        script.write('python %s -r %d -w %.4f -b %s -p >> %s\n'%(pyscriptname, run_num, window_size, bkg_method, logfilename))
+                    else:
+                        script.write('python %s -r %d -w %d -b %s -p >> %s\n' % (
+                        pyscriptname, run_num, window_size, bkg_method, logfilename))
+                else:
+                    #script.write('srun -n 1 -C haswell python %s -r %d -w %d -b %s >> %s\n'%(pyscriptname, run_num, window_size, bkg_method, logfilename))
+                    if window_size<1:
+                        script.write('python %s -r %d -w %.4f -b %s >> %s\n' % (
+                        pyscriptname, run_num, window_size, bkg_method, logfilename))
+                    else:
+                        script.write('python %s -r %d -w %d -b %s >> %s\n'%(pyscriptname, run_num, window_size, bkg_method, logfilename))
+
+                script.write('pwd \n')
+                script.write('whoami \n')
+                script.write('date \n')
+
+            with open(scriptfullname, 'w') as condor_script:
+                condor_script.write('Universe  = vanilla \n')
+                condor_script.write('Executable = {} \n'.format(scriptfullname))
+                condor_script.write('Log = " {} \n'.format(os.path.join(script_dir, 'qsub_%s.log' % scriptname)))
+                condor_script.write('Error =  {} \n'.format(os.path.join(script_dir, 'qsub_%s.err' % scriptname)))
+                condor_script.write('Output =  {} \n'.format(os.path.join(script_dir, 'qsub_%s.out' % scriptname)))
+                condor_script.write('should_transfer_files = YES \n')
+                condor_script.write('WhenToTransferOutput = ON_EXIT \n')
+                #condor_script.write('Requirements = (machine == \"tehanu.nevis.columbia.edu\" || machine== \"ged.nevis.columbia.edu\" || machine== \"serret.nevis.columbia.edu\") \n')
+                condor_script.write('Requirements = (machine== \"ged.nevis.columbia.edu\" || machine== \"serret.nevis.columbia.edu\") \n')
+                condor_script.write('Notification = NEVER \n')
+                condor_script.write('getenv = True \n')
+                condor_script.write('Queue \n')
+
+            #isend_command = 'qsub -l nodes=reedbuck -q batch -V %s'%scriptfullname
+            #isend_command = 'qsub -l nodes=%s -q batch -V %s'%(hostname, scriptfullname)
+            #isend_command = 'sbatch -C haswell %s'%(scriptfullname)
+            isend_command = 'condor_submit {}'.format(scriptfullname)
 
 
 
